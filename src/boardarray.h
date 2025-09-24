be8729e4d5da160fd20e5de412c5d08f48942a41
Roland James <rolandjamessalvador56@gmail.com>
2025-09-24 10:21:36 +0800

    Update BoardArray implementation

--- src/boardarray.h
+++ src/boardarray.h
@@ -13,16 +13,39 @@ class BoardArray : public Board {
             index = 0;
         }
 
-        void add(Entry* entry) {
-            // TODO: IMPLEMENT THIS FUNCTION
-            // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
-
+    void add(Entry* entry) override {
+    int pos = index; 
+    for (int i = 0; i < index; i++) {
+        Entry* current = array + i; 
+        if (entry->compare(current)) { 
+            pos = i;
+            break;
         }
+    }
 
-        void print() {
-            for (int i = 0; i < index; i++) {
-                cout << i + 1 << ". ";
-                array[i].print();
+    if (index < SIZE) {
+       
+        for (int j = index; j > pos; j--) {
+            array[j] = array[j - 1];
+        }
+        array[pos] = *entry;
+        index++;
+    } else {
+        if (pos < SIZE) {
+            for (int j = SIZE - 1; j > pos; j--) {
+                array[j] = array[j - 1];
             }
+            array[pos] = *entry;
+        } else {
+            cout << entry->name << "'s score is too low to be added!\n";
         }
+    }
+}
+
+    void print() override {
+        for (int i = 0; i < index; i++) {
+            cout << i + 1 << ". ";
+            array[i].print();
+        }
+    }
 };
